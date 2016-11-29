ee237b73652c2a6b3b39a9067fa375e4068058c3
Joe McAlister <joe@lonestorm.com>
2016-11-29 15:25:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  opency barebones
+//
+//  Created by Joe on 27/11/2016.
+//  Copyright © 2016 joemcalister. All rights reserved.
+//
+
+#include <unistd.h>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+
+int main( void ){
+    
+    // note finished
+    fprintf(stderr, "\nEmpty project.\n\n");
+    
+    // end
+    return(0);
+}
